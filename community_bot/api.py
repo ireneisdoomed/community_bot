0d27bb97a560708ea34628207ba921ffb40cd3ec
Irene López <irene.lopezs@protonmail.com>
2022-03-26 23:45:25 +0100

    Control status code in new_post_hook

--- community_bot/api.py
+++ community_bot/api.py
@@ -1,11 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 
 from community_bot.schemas import *
 
 app = FastAPI()
 
 
-@app.post("/new_post_hook", response_model=InNewPost)
+@app.post("/new_post_hook", response_model=InNewPost, status_code=status.HTTP_200_OK)
 async def listener(payload: InNewPost):
     print(payload)
     '''
@@ -26,5 +26,3 @@ async def listener(payload: InNewPost):
 @app.get("/items/{item_id}")
 async def read_item(item_id):
     return {"item_id": item_id}
-
-# TODO: create status endpoint
