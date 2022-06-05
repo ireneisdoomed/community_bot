1c97313c58c59e5c5ee6cded81ba655550ec4495
Irene López <irene.lopezs@protonmail.com>
2022-06-05 20:46:34 +0100

    Added method to fetch latest posts

--- community_bot/main.py
+++ community_bot/main.py
@@ -1,28 +1,47 @@
+from dotenv import load_dotenv
+import os
+
 from fastapi import FastAPI, status
+import requests
 
 from community_bot.schemas import *
 
-app = FastAPI()
+load_dotenv()
+
+app = FastAPI(
+    title="Bot of the Open Targets Community",
+    version="0.1.0",
+)
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to the Open Targets Community Bot!"}
 
 
 @app.post("/new_post_hook", response_model=InNewPost, status_code=status.HTTP_200_OK)
 async def new_post_hook(payload: InNewPost):
     print(payload)
-    '''
-    title = payload['title']
 
-    if title == 'test':
-        prediction = model.predict(title)
-    
-    req = {
-        "prediction": prediction,
-    }
-
-    requests.req(endpoint_slack)
-    '''
     return payload
 
 
-@app.get("/greet/{name}")
-async def say_hi(name: str):
-    return {"greeting": f'hi {name}'}
+@app.get("/latest_posts", status_code=status.HTTP_200_OK)
+async def latest_posts():
+
+    res = get_latest_posts()
+
+    # TODO: Process the response to only return the data we want
+    
+    return res['topic_list']['topics'][0]
+
+def get_latest_posts():
+    """
+    Returns the top 200 latest posts from the Discourse API.
+    """
+
+    root = 'https://community.opentargets.org/latest.json'
+    params = {
+        'api_username': os.environ.get('API_USERNAME'),
+        'api_key': os.environ.get('API_KEY'),
+    }
+    return requests.get(root, params=params).json()
